5ed64484abf7473ffd22289313013a041785c25a
marykang09 <mary.kang09@gmail.com>
2020-03-11 16:16:50 -0400

    my pets lab

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  def initialize (name)
+    @name = name
+    @species = "human"
+    
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+   binding.pry
+  end
+
+
 end
