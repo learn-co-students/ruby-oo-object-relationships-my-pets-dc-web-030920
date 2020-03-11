52b3b99eab463548851b0cb4d228596c8361af54
musiccity365 <m_sackstein@yahoo.com>
2020-03-11 16:17:31 -0400

    updates

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  attr_accessor :say_species
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+  end
+
+  def say_species
+    @say_species = "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all << name
+    return @@all
+  end
 end
