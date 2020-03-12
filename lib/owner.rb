846867e615958475112226ee13216439161d77f5
itacamba <daniela.sahagun@hotmail.com>
2020-03-11 22:29:02 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 class Owner
   
@@ -32,5 +32,44 @@ class Owner
   def cats 
     Cat.all.find_all{|cat| cat.owner == self}
   #binding.pry
+  end
+
+  def dogs
+    Dog.all.find_all{|dog| dog.owner == self} #returns an array of all instances
+  end
+
+  def pets
+    cats + dogs
+  end
+
+  def self.count
+    @@all_owners.length
+  end
+
+  def buy_cat(cat_name)
+    new_cat = Cat.new(cat_name,self)
+  end
+
+  def buy_dog(dog_name)
+    new_dog = Dog.new(dog_name, self) #you have to make the new dog buyable
+  end
+  
+  def walk_dogs
+    self.dogs.each { |dog| dog.mood = "happy" }
+  end
+
+  def feed_cats
+    self.cats.each { |cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.pets.each do |pet| 
+      pet.mood = "nervous"
+      pet.owner = nil
+  end
+
+  def list_pets
+    return "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
   end
 end
